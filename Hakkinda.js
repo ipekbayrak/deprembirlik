b5c0be051a80b821df5aacdbd6e6e4fe12db71b0
ipekbayrak <mustafaipekbayrak@gmail.com>
2023-02-18 10:33:31 +0300

    uygulama adı düzenlendi

--- Hakkinda.js
+++ Hakkinda.js
@@ -6,16 +6,17 @@ const Hakkinda = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
-        Deprem Destek uygulamasına hoş geldiniz.
+        Deprem Birlik uygulamasına hoş geldiniz.
         {'\n\n'}
         Depremzedelere destek olmak amacıyla geliştirilen bu uygulama, ihtiyaç sahiplerine bilgi ve kaynak sağlamak amacıyla oluşturulmuştur.
         {'\n\n'}
         Herhangi bir öneriniz veya iyileştirmeniz varsa, lütfen Twitter (@ipekbayrak01) aracılığıyla irtibat kurmaktan çekinmeyin.
         {'\n\n'}
-        <code>github.com/ipekbayrak/depremdestek</code>
+        <code>github.com/ipekbayrak/deprembirlik</code>
         {'\n\n'}
         Bu uygulamanın depremzedelere yardımcı olacağını umuyorum.
       </Text>
+      <Text style={{ padding: 5 }}>Versiyon:0.1.1</Text>
     </View>
   );
 };
